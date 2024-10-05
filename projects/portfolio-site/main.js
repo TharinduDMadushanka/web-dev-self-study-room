7bca35aa01b37c8cd76c6ab594bb66724ee64a08
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-10-05 17:10:43 +0530

    start protfolio project & completed hero and about section

---
+++ projects/portfolio-site/main.js
@@ -0,0 +1,20 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    // Remove "active-link" class from all tab links
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    // Remove "active-tab" class from all tab contents
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    // Add the "active-link" class to the clicked tab
+    event.currentTarget.classList.add("active-link");
+
+    // Add the "active-tab" class to the tab content with the given id (tabname)
+    document.getElementById(tabname).classList.add("active-tab");
+}
